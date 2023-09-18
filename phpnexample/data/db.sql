d0fe2f46896938c77dcdca093141ea8fdf9a82e0
Will Wesley <will@knze.net>
2023-09-18 08:56:42 -0600

    machina for php/mysql demo

---
+++ phpnexample/data/db.sql
@@ -0,0 +1,6 @@
+create database db34;
+use db34;
+create table users ( name varchar(34), email varchar(34), password varchar(34) );
+insert into users values ("bob","bob@bob.com","secrets, wow"), ("lois","lois@lane.edu","2124rgwvwc");
+insert into users values ("ken","ken@barbieland.com","patriarchy"), ("joju","chewbacca@every.io","5jynrgb234tgrve");
+
