d321eb1e72d0f2f3125f306fb81dbcdeeadd9670
Will Wesley <will@knze.net>
2023-11-06 11:54:22 -0700

    Initial media thing

---
+++ pictures/media.js
@@ -0,0 +1,10 @@
+(() => {
+  let video = null;
+
+  function startup() {
+    video = document.getElementById("video");
+    // TODO
+  }
+
+  window.addEventListener("load", startup, false);
+})();
